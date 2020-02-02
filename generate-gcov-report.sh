6cf17c2608cbe679bd2299eeb33c0a82bced80b1
Falko Schwabe <falko.schwabe@gmail.com>
2020-02-02 03:00:43 +0100

    do not delete previous gcov reports

--- generate-gcov-report.sh
+++ generate-gcov-report.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 
 mkdir -p gcov
-rm -f gcov/*.gcov
 
 find . -name "*.gcno" \
     -exec sh -c "LC_ALL=en_US LANG=en_US gcov -abcflmp \${1%.gcno}.o > /dev/null" _ {} \;
 
-mv -t gcov *.gcov || true
+mv -ft gcov *.gcov || true
